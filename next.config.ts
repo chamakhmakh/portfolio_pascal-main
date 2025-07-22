33708c9e1c44cf4d0b00c7bea224c0a74b51e1b8
farouk <bensaidf232@gmail.com>
2025-07-22 03:12:37 +0100

    feat: add Next.js configuration options for build and image handling

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    unoptimized: true,
+  },
+  // Add any other needed configurations here
 };
 
 export default nextConfig;
